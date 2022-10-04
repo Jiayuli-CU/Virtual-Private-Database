c0cc6b43d83c561ee0fdb575b882360a81842fd2
ziruih <ziruih73@gmail.com>
2022-10-04 15:32:37 +0800

    update policy

--- create_policy_product.sql
+++ create_policy_product.sql
@@ -1,17 +1,18 @@
-CREATE FUNCTION check_ad_country(v_schema IN VARCHAR2, v_obj IN VARCHAR2)
+--- employees can only select his or her own data from Employee table.
+CREATE FUNCTION check_employee(v_schema IN VARCHAR2, v_obj IN VARCHAR2)
 RETURN VARCHAR2 AS condition VARCHAR2 (200); 
 BEGIN
-	condition := 'country = "ID"';
+	condition := 'ID = SYS_CONTEXT("USERENV", "SESSION_USER")';
 	RETURN condition;
-END check_ad_country;
+END check_employee;
 
 
 BEGIN
   DBMS_RLS.ADD_POLICY (
     object_schema => 'system',
-    object_name => 'advertisement',
-    policy_name => 'check_ad_country_policy',
-    policy_function => 'check_ad_country');
+    object_name => 'employees',
+    policy_name => 'check_employee_policy',
+    policy_function => 'check_employee');
 END;
 
 
