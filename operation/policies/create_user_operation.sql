eea3428387e7dcbd2a16b73d58246936123fce18
Lifang <82812238+lfzhang-413@users.noreply.github.com>
2022-10-09 18:17:44 +0800

    Update create_user_operation.sql

--- operation/policies/create_user_operation.sql
+++ operation/policies/create_user_operation.sql
@@ -18,6 +18,27 @@ GRANT SELECT ON PROJECTS TO content_operation;
 
 GRANT ALL ON EVENT_PLANNING TO activities_operation;
 
+GRANT SELECT ON Administration_Conference TO product_operation;
+GRANT SELECT ON Administration_Food TO product_operation;
+GRANT SELECT ON HR_Attendance TO product_operation;
+GRANT SELECT ON HR_Leave TO product_operation;
+GRANT SELECT, UPDATE ON HR_Person_Details TO product_operation;
+GRANT SELECT ON ACCOUNTING_SALARY_HISTORY TO product_operation;
+
+GRANT SELECT ON Administration_Conference TO content_operation;
+GRANT SELECT ON Administration_Food TO content_operation;
+GRANT SELECT ON HR_Attendance TO content_operation;
+GRANT SELECT ON HR_Leave TO content_operation;
+GRANT SELECT, UPDATE ON HR_Person_Details TO content_operation;
+GRANT SELECT ON ACCOUNTING_SALARY_HISTORY TO content_operation;
+
+GRANT SELECT ON Administration_Conference TO activities_operation;
+GRANT SELECT ON Administration_Food TO activities_operation;
+GRANT SELECT ON HR_Attendance TO activities_operation;
+GRANT SELECT ON HR_Leave TO activities_operation;
+GRANT SELECT, UPDATE ON HR_Person_Details TO activities_operation;
+GRANT SELECT ON ACCOUNTING_SALARY_HISTORY TO activities_operation;
+
 CREATE USER MK0001 IDENTIFIED BY abcd1234;
 CREATE USER MK0002 IDENTIFIED BY abcd1234;
 CREATE USER MK0003 IDENTIFIED BY abcd1234;
