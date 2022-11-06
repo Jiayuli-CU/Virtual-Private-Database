c1c1aa4620c2bdb708cd993ca1d85c0c426fcebb
YANGZ001 <zyfunnyyoung@gmail.com>
2022-11-06 15:54:52 +0800

    Update test databases and servers.

--- R&D/amc/test/test_databases.sql
+++ R&D/amc/test/test_databases.sql
@@ -1,26 +1,11 @@
--- SELECT
-SELECT * FROM system.projects;
-SELECT * FROM system.servers;
-SELECT * FROM system.databases;
-SELECT * FROM system.repositories;
+-- D0001 -- dev
+select * from cs5322.databases;
+select * from cs5322.servers;
 
--- SELECT
-SELECT * FROM system.databases;
--- UPDATE
-UPDATE system.databases SET region='TW' WHERE id='49';
--- DELETE
-DELETE FROM system.databases where id='49';
+-- D0002 -- dev+test
+select * from cs5322.databases;
+select * from cs5322.servers;
 
---For SDE_BE_1==D0001; S
---For SDE_BE_2==D0002; SU
---For SDE_BE_3==D0003; ALL
---For SDE_TE_1==D0007; S
---For SDE_TE_2==D0008; SU
---For SDE_TE_3==D0009; ALL
-
-SELECT * from system.ACCOUNTING_SALARY_HISTORY;
-SELECT * from system.HR_Person_Details;
-SELECT * from system.HR_Attendance;
-SELECT * from system.HR_Leave;
-SELECT * from system.Administration_Conference;
-SELECT * from system.Administration_Food;
+-- D0003 -- dev+test+prod
+select * from cs5322.databases;
+select * from cs5322.servers;
