9d1bdb3bb3ed963d377e9bb92eaab0e6cdd26b01
YANGZ001 <zyfunnyyoung@gmail.com>
2022-11-06 15:32:11 +0800

    Update MAC for databases and servers.

--- R&D/amc/create/create_table_servers.sql
+++ R&D/amc/create/create_table_servers.sql
@@ -1,27 +1,3 @@
-DECLARE
-BEGIN
-EXECUTE IMMEDIATE 'CREATE TABLE servers
-                   (
-                       id INT NOT NULL,
-                       name VARCHAR(32) UNIQUE NOT NULL,
-                       ip VARCHAR(128) UNIQUE NOT NULL,
-                       access_level CHAR(2) CHECK(access_level IN (''p0'',''p1'',''p2'', ''p3'')) NOT NULL,
-                       create_time DATE NOT NULL,
-                       update_time DATE NOT NULL,
-                       status CHAR(16) DEFAULT ON NULL (''active'') CHECK(status IN (''active'', ''down'',''deprecate'')),
-                       region VARCHAR(16) NOT NULL,
-                       environment CHAR(8) CHECK(environment IN (''test'', ''prod'')) NOT NULL,
-                       repository_id INT NOT NULL,
-                       CONSTRAINT servers_id_fk FOREIGN KEY (repository_id) REFERENCES repositories (id),
-                       CONSTRAINT servers_id_pk PRIMARY KEY (id)
-                   )';
-EXCEPTION WHEN OTHERS THEN
-        IF SQLCODE = -955 THEN NULL ;
-ELSE RAISE ;
-END IF;
-END;
-
-
 DROP TABLE servers;
 CREATE TABLE servers
 (
@@ -29,12 +5,10 @@ CREATE TABLE servers
     name VARCHAR(32) NOT NULL,
     ip VARCHAR(128) UNIQUE NOT NULL,
     access_level CHAR(2) CHECK(access_level IN ('p0','p1','p2', 'p3')) NOT NULL,
-    create_time DATE NOT NULL,
-    update_time DATE NOT NULL,
     status CHAR(16) DEFAULT ON NULL ('active') CHECK(status IN ('active', 'down','deprecate')),
     region VARCHAR(16) NOT NULL,
     repository_id INT NOT NULL,
-    environment CHAR(8) CHECK(environment IN ('test', 'prod')) NOT NULL,
+    environment CHAR(8) CHECK(environment IN ('test', 'prod', 'dev')) NOT NULL,
     CONSTRAINT servers_id_fk FOREIGN KEY (repository_id) REFERENCES repositories (id),
     CONSTRAINT servers_id_pk PRIMARY KEY (id)
 );
---
+++ R&D/amc/policies/servers_policy.sql
@@ -0,0 +1,162 @@
+---- for servers
+GRANT SELECT ON servers TO SDE_BE_3;
+GRANT SELECT ON servers TO SDE_TE_3;
+GRANT SELECT ON servers TO SDE_FE_3;
+GRANT SELECT ON servers TO SDE_BE_2;
+GRANT SELECT ON servers TO SDE_TE_2;
+GRANT SELECT ON servers TO SDE_FE_2;
+GRANT SELECT ON servers TO SDE_BE_1;
+GRANT SELECT ON servers TO SDE_TE_1;
+GRANT SELECT ON servers TO SDE_FE_1;
+
+BEGIN
+  SA_SYSDBA.DROP_POLICY
+(
+    policy_name  => 'SERVERS_POL',
+    drop_column  => TRUE);
+END;
+/
+
+
+BEGIN
+ SA_SYSDBA.CREATE_POLICY
+(
+  policy_name      => 'SERVERS_POL',
+  column_name      => 'SERVERS_OLS_COL');
+END;
+/
+
+EXEC SA_SYSDBA.ENABLE_POLICY ('SERVERS_POL');
+
+BEGIN
+   SA_COMPONENTS.CREATE_LEVEL
+(
+      policy_name => 'SERVERS_POL',
+      level_num   => 3002,
+      short_name  => 'HS',
+      long_name   => 'HIGHLY_SENSITIVE');
+
+   SA_COMPONENTS.CREATE_LEVEL
+(
+      policy_name => 'SERVERS_POL',
+      level_num   => 2502,
+      short_name  => 'S',
+      long_name   => 'SENSITIVE');
+
+    SA_COMPONENTS.CREATE_LEVEL
+(
+      policy_name => 'SERVERS_POL',
+      level_num   => 2002,
+      short_name  => 'PS',
+      long_name   => 'PARTIALLY_SENSITIVE');
+
+   SA_COMPONENTS.CREATE_LEVEL
+(
+      policy_name => 'SERVERS_POL',
+      level_num   => 1502,
+      short_name  => 'IS',
+      long_name   => 'INSENSITIVE');
+END;
+/
+
+
+BEGIN
+    SA_USER_ADMIN.SET_LEVELS
+(
+      policy_name  => 'SERVERS_POL',
+      user_name    => 'D0001',
+      max_level    => 'PS',
+      min_level    => 'IS');
+   SA_USER_ADMIN.SET_LEVELS
+(
+      policy_name  => 'SERVERS_POL',
+      user_name    => 'D0002',
+      max_level    => 'S',
+      min_level    => 'IS');
+       SA_USER_ADMIN.SET_LEVELS
+(
+      policy_name  => 'SERVERS_POL',
+      user_name    => 'D0003',
+      max_level    => 'HS',
+      min_level    => 'IS');
+    SA_USER_ADMIN.SET_LEVELS
+(
+      policy_name  => 'SERVERS_POL',
+      user_name    => 'D0004',
+      max_level    => 'PS',
+      min_level    => 'IS');
+   SA_USER_ADMIN.SET_LEVELS
+(
+      policy_name  => 'SERVERS_POL',
+      user_name    => 'D0005',
+      max_level    => 'S',
+      min_level    => 'IS');
+       SA_USER_ADMIN.SET_LEVELS
+(
+      policy_name  => 'SERVERS_POL',
+      user_name    => 'D0006',
+      max_level    => 'HS',
+      min_level    => 'IS');
+    SA_USER_ADMIN.SET_LEVELS
+(
+      policy_name  => 'SERVERS_POL',
+      user_name    => 'D0007',
+      max_level    => 'PS',
+      min_level    => 'IS');
+   SA_USER_ADMIN.SET_LEVELS
+(
+      policy_name  => 'SERVERS_POL',
+      user_name    => 'D0008',
+      max_level    => 'S',
+      min_level    => 'IS');
+       SA_USER_ADMIN.SET_LEVELS
+(
+      policy_name  => 'SERVERS_POL',
+      user_name    => 'D0009',
+      max_level    => 'HS',
+      min_level    => 'IS');
+END;
+
+BEGIN
+  SA_POLICY_ADMIN.APPLY_TABLE_POLICY
+(
+    policy_name    => 'SERVERS_POL',
+    schema_name    => 'CS5322',
+    table_name     => 'DATABASES',
+    table_options  => 'READ_CONTROL');
+END;
+/
+
+BEGIN
+   SA_POLICY_ADMIN.ENABLE_TABLE_POLICY
+(
+      policy_name => 'SERVERS_POL',
+      schema_name => 'CS5322',
+      table_name  => 'DATABASES');
+END;
+/
+
+BEGIN
+   SA_USER_ADMIN.SET_USER_PRIVS
+(
+      policy_name => 'SERVERS_POL',
+      user_name   => 'CS5322',
+      privileges  => 'FULL');
+END;
+/
+
+UPDATE cs5322.DATABASES
+SET DB_OLS_COL = CHAR_TO_LABEL('SERVERS_POL', 'HS')
+WHERE status = 'prod';
+
+UPDATE cs5322.DATABASES
+SET DB_OLS_COL = CHAR_TO_LABEL('SERVERS_POL', 'S')
+WHERE status = 'test';
+
+UPDATE cs5322.DATABASES
+SET DB_OLS_COL = CHAR_TO_LABEL('SERVERS_POL', 'PS')
+WHERE status = 'dev';
+
+
+
+
