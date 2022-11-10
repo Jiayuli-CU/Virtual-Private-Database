e469cf55a502f6e08cbf0ca07c4b1abf964d6250
YANGZ001 <zyfunnyyoung@gmail.com>
2022-11-10 10:59:54 +0800

    Fix for db and servers

--- R&D/amc/test/test_databases.sql
+++ R&D/amc/test/test_databases.sql
@@ -1,5 +1,7 @@
 -- D0001 -- dev
 select * from cs5322.databases;
+delete from cs5322.databases where id =4;
+
 select * from cs5322.servers;
 
 -- D0002 -- dev+test
@@ -9,3 +11,5 @@ select * from cs5322.servers;
 -- D0003 -- dev+test+prod
 select * from cs5322.databases;
 select * from cs5322.servers;
+
+delete from cs5322.databases where id =1;
